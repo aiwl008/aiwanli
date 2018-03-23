9a8d0ac9206b80492227c50f1b34d74594d90e7c
aiwanli <aiwanlicom@126.com>
2018-03-23 17:16:49 +0800

    提交

---
+++ src/main/java/com/aiwl/pms/controller/login/LoginController.java
@@ -0,0 +1,37 @@
+package com.aiwl.pms.controller.login;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.aiwl.common.utils.RequestUtil;
+import com.aiwl.pms.entity.Student;
+import com.aiwl.pms.service.StudentService;
+
+@Controller
+public class LoginController {
+	
+	@Autowired
+	private StudentService studentService;
+	
+	@RequestMapping(value="login",method = RequestMethod.GET)
+	public String saveStudent(HttpServletRequest request,HttpServletResponse response,String userName,String password){
+//        password = DigestUtils.md5Hex(password);
+//        User user = userService.findUser(userName,password);
+        Student user =  studentService.findStudent(userName,password);
+        if(user!=null){
+                request.getSession().setAttribute("userId", user.getId());  
+                request.getSession().setAttribute("user", userName);  
+                
+                return "redirect:" + RequestUtil.retrieveSavedRequest(request);//跳转至访问页面
+        }else{
+               request.getSession().setAttribute("message", "用户名不存在，请重新登录");
+               
+               return "index"; 
+        }
+	}
+}
